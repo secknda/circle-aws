348133a4ae4dbf5ee75222b8ba648093624ce004
secknda <baba98seck@gmail.com>
2023-01-16 13:44:42 +0000

    second commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "devops-tf-circleci"
+    key    = "platform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-06878d265978313ca"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-09a1d2e51ea27438d"
+}
